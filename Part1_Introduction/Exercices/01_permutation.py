cfb161c0493f430aa7bb37ef923bdab09c90f65e
Antoine07 <antoine.lucsko@gmail.com>
2023-11-22 10:35:30 +0100

    permutation

--- Part1_Introduction/Exercices/01_permutation.py
+++ Part1_Introduction/Exercices/01_permutation.py
@@ -13,3 +13,17 @@ b = 3
 # assignation en utilisant des tuples
 a, b = b, a
 print(f"a :{a} b : {b}")
+
+a = 1
+b = 2
+c = 3
+
+t = a 
+a = b 
+b = c
+c = t
+print(f"a :{a} b : {b} c {c}")
+
+a, b, c = 1, 2, 3
+a, b, c = b, c, a
+print(f"a :{a} b : {b} c {c}")
